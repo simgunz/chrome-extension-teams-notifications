b5ebedf948ced9185e23e9259875710b0c6e4638
Ján Halaša <jan.halasa@sigp.eu>
2023-02-25 20:31:31 +0100

    Extension options

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,7 +1,34 @@
-// https://stackoverflow.com/a/9517879
-const s = document.createElement('script');
-s.src = chrome.runtime.getURL('scripts/teams-notification.js');
-s.onload = function () {
-  this.remove();
+const teamsExtension = {
+  options: undefined
+}
+
+const sendOptionsToApplicationWindow = () => {
+  console.debug('Sending extension options to application window', teamsExtension.options);
+  window.postMessage({type : 'teams-extension-options', text: JSON.stringify(teamsExtension.options)}, "*");
+}
+
+const createTeamsNotificationScript = () => {
+  // https://stackoverflow.com/a/9517879
+  const s = document.createElement('script');
+  s.src = chrome.runtime.getURL('scripts/teams-notification.js');
+  s.onload = function () {
+    sendOptionsToApplicationWindow();
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
 };
-(document.head || document.documentElement).appendChild(s);
+
+chrome.storage.sync.get(defaultOptions, optionsFromStorageOrDefaults => {
+  teamsExtension.options = optionsFromStorageOrDefaults;
+  console.debug('Teams extension options', teamsExtension.options);
+
+  createTeamsNotificationScript();
+});
+
+chrome.storage.onChanged.addListener((changes) => {
+  console.log('Extension options changed:', changes);
+  chrome.storage.sync.get(defaultOptions, optionsFromStorageOrDefaults => {
+    teamsExtension.options = optionsFromStorageOrDefaults;
+    sendOptionsToApplicationWindow();
+  });
+});
